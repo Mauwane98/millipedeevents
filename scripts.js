d218a7e4f565f02f0c96854dcd77f3cd8ebde3eb
5asitgroup <137446224+5asitgroup@users.noreply.github.com>
2024-08-16 12:57:06 +0200

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('nav ul');
+
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
